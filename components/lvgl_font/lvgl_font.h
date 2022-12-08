ead90c1e25e0624e4e2cb60cdb1a8003357c6d24
phb98 <phanhaibac98@gmail.com>
2022-12-09 00:52:27 +0700

    Add some fonts

--- components/lvgl_font/lvgl_font.h
+++ components/lvgl_font/lvgl_font.h
@@ -1,4 +1,14 @@
 #ifndef _LVGL_FONT_H_
 #define _LVGL_FONT_H_
-LV_FONT_DECLARE(seven_seg_14_number)
+LV_FONT_DECLARE(seven_seg_14)
+LV_FONT_DECLARE(seven_seg_16)
+LV_FONT_DECLARE(seven_seg_18)
+LV_FONT_DECLARE(seven_seg_20)
+LV_FONT_DECLARE(seven_seg_22)
+LV_FONT_DECLARE(seven_seg_24)
+LV_FONT_DECLARE(seven_seg_26)
+LV_FONT_DECLARE(seven_seg_2_16)
+
+
+
 #endif
